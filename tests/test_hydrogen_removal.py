9f91e88ab51787a580fa28184532f439df1aa41a
Mike Henry <mikehenry@boisestate.edu>
2019-06-06 22:55:02 +0000

    Merged in mikemhenry/planckton (pull request #25)
    switch to a fork until mbuild merges it
    
    * switch to a fork until mbuild merges it
    
    * formatting fix for black
    
    Approved-by: Mike Henry <mikehenry@boisestate.edu>

--- tests/test_hydrogen_removal.py
+++ tests/test_hydrogen_removal.py
@@ -1,4 +1,5 @@
 from planckton.init import Compound, Pack
+from planckton.sim import Simulation
 from planckton.compounds import COMPOUND_FILE
 from planckton.force_fields import FORCE_FIELD
 from os import path, remove
@@ -24,6 +25,31 @@ def test_hydrogen_removal():
             "_h1",
             "_h4",
         ], "Hydrogen found in system!"
+
+
+def test_hydrogen_removal_and_sim():
+    pcbm = Compound(COMPOUND_FILE["PCBM"])
+    packer = Pack(
+        pcbm,
+        ff_file=FORCE_FIELD["opv_gaff"],
+        n_compounds=2,
+        density=0.01,
+        out_file="test_init.hoomdxml",
+        remove_hydrogen_atoms=True,
+    )
+
+    packer.pack()
+    my_sim = Simulation(
+        "test_init.hoomdxml",
+        kT=3.0,
+        gsd_write=1e2,
+        log_write=1e2,
+        e_factor=0.5,
+        n_steps=3e3,
+        mode="cpu",
+        shrink_time=1e3,
+    )
+    my_sim.run()
 
 
 if __name__ == "__main__":
