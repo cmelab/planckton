24ccf6315c76c5d739452a0a2620e18c30b8b110
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2020-10-21 17:26:16 -0600

    add unit system and remove conversion factors

--- planckton/utils/base_units.py
+++ planckton/utils/base_units.py
@@ -1,14 +1,14 @@
-def base_units():
-    units = {}
-    units["avogadro"] = 6.022140857e23
-    units["boltzmann"] = 1.38064852e-23
-    units["kj_to_j"] = 1e3
-    units["amu_to_kg"] = 1.6605e-27
-    units["nm_to_m"] = 1e-9
-    units["mass"] = 32.06
-    units["mass_units"] = "amu"
-    units["energy"] = 1.046
-    units["energy_units"] = "kj/mol"
-    units["distance"] = 0.35635948725613575
-    units["distance_units"] = "nm"
-    return units
+import unyt as u
+
+planckton_units = u.UnitSystem(
+        'planckton', mass_unit='amu', length_unit='nm', time_unit='s'
+        )
+planckton_units['energy'] = 'kJ'
+
+base_units = {
+        "avogadro": 6.022140857e23 / u.mol,
+        "boltzmann": 1.38064852e-23 * u.J / u.K,
+        "mass": (32.06 * u.amu).in_base('planckton'),
+        "energy": (1.046 * u.kJ / u.mol).in_base('planckton'),
+        "length": (0.35635948725613575 * u.nm).in_base('planckton'),
+        }
