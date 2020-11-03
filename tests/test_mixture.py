c3ce63d83e940ebeb99141db4ee6202abca9b15d
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2020-11-03 12:06:22 -0700

    add a test for a compound mixture

---
+++ tests/test_mixture.py
@@ -0,0 +1,37 @@
+from os import path, remove
+
+from planckton.compounds import COMPOUND_FILE
+from planckton.force_fields import FORCE_FIELD
+from planckton.init import Compound, Pack
+from planckton.sim import Simulation
+
+
+def test_mixture():
+    pcbm = Compound(COMPOUND_FILE["PCBM"])
+    p3ht = Compound(COMPOUND_FILE["P3HT"])
+    packer = Pack(
+        [pcbm, p3ht],
+        ff_file=FORCE_FIELD["opv_gaff"],
+        n_compounds=[2,3],
+        density=0.01,
+        out_file="test_init.hoomdxml",
+    )
+
+    packer.pack()
+    my_sim = Simulation(
+        "test_init.hoomdxml",
+        kT=3.0,
+        gsd_write=1e2,
+        log_write=1e2,
+        e_factor=0.5,
+        n_steps=3e3,
+        mode="cpu",
+        shrink_time=1e3,
+    )
+    my_sim.run()
+
+
+if __name__ == "__main__":
+    if path.isfile("restart.gsd"):
+        remove("restart.gsd")
+    test_hydrogen_removal()
