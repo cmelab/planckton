c92a73049f33a2cf07873fdac492f59f57a22461
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2020-11-06 16:25:02 -0700

    change path to dir

--- planckton/compounds/__init__.py
+++ planckton/compounds/__init__.py
@@ -1,7 +1,8 @@
 import os
 import glob
+from pathlib import Path
 
-COMPOUND_DIR = os.path.abspath(os.path.dirname(__file__))
+COMPOUND_DIR = Path(__file__).parent / "compounds"
 COMPOUND_FILE = {}
 for compound_file in glob.glob(os.path.join(COMPOUND_DIR, "*_typed.mol2")):
     # The the last part of the path, drop the _typed.mol2
