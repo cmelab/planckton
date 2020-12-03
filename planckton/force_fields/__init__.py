afab018f596f01f8653479e4096d576e2d0809ee
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2020-12-03 11:59:03 -0700

    forcefield dict passes a forcefield obj instead of a path to xml

--- planckton/force_fields/__init__.py
+++ planckton/force_fields/__init__.py
@@ -1,10 +1,12 @@
 import os
 import glob
 
+from foyer import Forcefield
+
 FORCE_FIELD_DIR = os.path.abspath(os.path.dirname(__file__))
 FORCE_FIELD = {}
 for force_field_file in glob.glob(os.path.join(FORCE_FIELD_DIR, "*/*.xml")):
     force_field_name = force_field_file.split("/")[-1].split(".xml")[0]
-    FORCE_FIELD[force_field_name] = force_field_file
+    FORCE_FIELD[force_field_name] = Forcefield(force_field_file)
 
 print(FORCE_FIELD)
