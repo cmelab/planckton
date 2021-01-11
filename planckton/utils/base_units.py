1d91165b0cc6dddefb444f68db290146fa7afca3
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-01-11 13:52:47 -0700

    autoscale is used so base units no longer necessary

--- planckton/utils/base_units.py
+++ planckton/utils/base_units.py
@@ -5,12 +5,7 @@ planckton_units = u.UnitSystem(
         )
 planckton_units['energy'] = 'kJ'
 
-# base units are for sulfur
-# TODO should we use autoscale option instead??
-base_units = {
+constants= {
         "avogadro": 6.022140857e23 / u.mol,
         "boltzmann": 1.38064852e-23 * u.J / u.K,
-        "mass": (32.06 * u.amu).in_base('planckton'),
-        "energy": (1.046 * u.kJ / u.mol).in_base('planckton'),
-        "length": (0.35635948725613575 * u.nm).in_base('planckton'),
         }
