03fedd4f36b51fddf00e1e2bb57a43801d9e5cd2
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-01-22 15:42:21 -0700

    combine base units and unit conversions into units

--- planckton/utils/units.py
+++ planckton/utils/units.py
@@ -1,4 +1,15 @@
-from planckton.utils.base_units import constants
+import unyt as u
+
+
+planckton_units = u.UnitSystem(
+        'planckton', mass_unit='amu', length_unit='nm', time_unit='s'
+        )
+planckton_units['energy'] = 'kJ'
+
+constants= {
+        "avogadro": 6.022140857e23 / u.mol,
+        "boltzmann": 1.38064852e-23 * u.J / u.K,
+        }
 
 
 def reduced_from_kelvin(T_SI, ref_energy):
