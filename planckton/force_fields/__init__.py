ef4cdfdb71c74f9eb9db7e27b8f5d6d519d2ccba
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-01-26 13:18:07 -0700

    make the forcefield specification a little more clear

--- planckton/force_fields/__init__.py
+++ planckton/force_fields/__init__.py
@@ -1,10 +1,13 @@
 import os
-import glob
 
 from foyer import Forcefield
 
-FORCE_FIELD_DIR = os.path.abspath(os.path.dirname(__file__))
-FORCE_FIELD = {}
-for force_field_file in glob.glob(os.path.join(FORCE_FIELD_DIR, "*/*.xml")):
-    force_field_name = force_field_file.split("/")[-1].split(".xml")[0]
-    FORCE_FIELD[force_field_name] = Forcefield(force_field_file)
+FF_DIR = os.path.abspath(os.path.dirname(__file__))
+FORCE_FIELD = {
+        "opv_gaff": Forcefield(
+            os.path.join(FF_DIR, "gaff/opv_gaff.xml")
+            ),
+        "oplsua-custom": Forcefield(
+            os.path.join(FF_DIR, "oplsua/opls-custom.xml")
+            ),
+        }
