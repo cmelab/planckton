312b3ee11cd65ddbb58a07b35ccf20df34ac52c1
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-01-26 17:47:50 -0700

    add warnings wrapper for forcefields

--- planckton/force_fields/__init__.py
+++ planckton/force_fields/__init__.py
@@ -1,13 +1,12 @@
-import os
+from os.path import join
+import warnings
 
 from foyer import Forcefield
 
 FF_DIR = os.path.abspath(os.path.dirname(__file__))
-FORCE_FIELD = {
-        "opv_gaff": Forcefield(
-            os.path.join(FF_DIR, "gaff/opv_gaff.xml")
-            ),
-        "oplsua-custom": Forcefield(
-            os.path.join(FF_DIR, "oplsua/opls-custom.xml")
-            ),
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    FORCE_FIELD = {
+            "opv_gaff": Forcefield(join(FF_DIR, "gaff/opv_gaff.xml")),
+            "oplsua-custom": Forcefield(join(FF_DIR, "oplsua/opls-custom.xml")),
         }
