04075f990b6ae45e8847726a4dc7b8b19eef6bb1
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-01-26 17:56:11 -0700

    oops messed up os import

--- planckton/force_fields/__init__.py
+++ planckton/force_fields/__init__.py
@@ -1,12 +1,14 @@
-from os.path import join
+from os import path
 import warnings
 
 from foyer import Forcefield
 
-FF_DIR = os.path.abspath(os.path.dirname(__file__))
+FF_DIR = path.abspath(path.dirname(__file__))
 with warnings.catch_warnings():
     warnings.simplefilter("ignore")
     FORCE_FIELD = {
-            "opv_gaff": Forcefield(join(FF_DIR, "gaff/opv_gaff.xml")),
-            "oplsua-custom": Forcefield(join(FF_DIR, "oplsua/opls-custom.xml")),
+            "opv_gaff": Forcefield(path.join(FF_DIR, "gaff/opv_gaff.xml")),
+            "oplsua-custom": Forcefield(
+                path.join(FF_DIR, "oplsua/opls-custom.xml")
+                ),
         }
