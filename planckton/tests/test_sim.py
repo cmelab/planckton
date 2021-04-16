13d5b1d88f04e99acfe4daf90e997829deba2b63
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-04-16 17:17:54 -0600

    add tests to check for those errors

--- planckton/tests/test_sim.py
+++ planckton/tests/test_sim.py
@@ -64,3 +64,23 @@ class TestSimulations(BaseTest):
                 density=0.01 * u.g / u.cm**3,
                 remove_hydrogen_atoms=True
             )
+
+    def test_smiles_opvgaff_raises(self):
+        p3ht = Compound("c1cscc1CCCCCC")
+        with pytest.raises(NotImplementedError):
+            packer = Pack(
+                p3ht,
+                ff=FORCE_FIELD["opv_gaff"],
+                n_compounds=2,
+                density=0.01 * u.g / u.cm**3,
+            )
+
+    def test_typed_gaff_raises(self):
+        p3ht = Compound(COMPOUND_FILE["P3HT_16"])
+        with pytest.raises(NotImplementedError):
+            packer = Pack(
+                p3ht,
+                ff=FORCE_FIELD["gaff"],
+                n_compounds=2,
+                density=0.01 * u.g / u.cm**3,
+            )
