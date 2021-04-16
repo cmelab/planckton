bd59e624da40a0b54d613ef4ac7d684eb380ccc0
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-04-16 17:51:34 -0600

    rename dictionaries to no hyphen

--- planckton/forcefields/__init__.py
+++ planckton/forcefields/__init__.py
@@ -8,7 +8,7 @@ from foyer import Forcefield
 FF_DIR = path.abspath(path.dirname(__file__))
 with warnings.catch_warnings():
     warnings.simplefilter("ignore")
-    FORCE_FIELD = {
+    FORCEFIELD = {
         "gaff-custom": Forcefield(path.join(FF_DIR, "gaff/opv_gaff.xml")),
         "oplsua-custom": Forcefield(path.join(FF_DIR, "oplsua/oplsua.xml")),
         "gaff": ff.load_GAFF(),
