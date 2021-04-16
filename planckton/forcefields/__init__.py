e66c7f162a0510360d33055d4c306d205291b811
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-04-16 17:28:57 -0600

    rename ff dir, make ff names more clear that they are custom

--- planckton/forcefields/__init__.py
+++ planckton/forcefields/__init__.py
@@ -9,9 +9,7 @@ FF_DIR = path.abspath(path.dirname(__file__))
 with warnings.catch_warnings():
     warnings.simplefilter("ignore")
     FORCE_FIELD = {
-        "opv_gaff": Forcefield(path.join(FF_DIR, "gaff/opv_gaff.xml")),
-        "oplsua-custom": Forcefield(
-            path.join(FF_DIR, "oplsua/opls-custom.xml")
-        ),
+        "gaff-custom": Forcefield(path.join(FF_DIR, "gaff/opv_gaff.xml")),
+        "oplsua-custom": Forcefield(path.join(FF_DIR, "oplsua/oplsua.xml")),
         "gaff": ff.load_GAFF(),
     }
